9ae9c04c1b7e794d4be8d4d54decaf39476dbc07
Michael Lockwood <lockwm@uw.edu>
2016-09-28 11:18:59 -0700

    Final settings upgrade.

--- apps/go_django/go_django/settings.py
+++ apps/go_django/go_django/settings.py
@@ -21,6 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 ALLOWED_HOSTS = ['*']
